2ca7c92a1176c4f56c76a0a1dcdd39fe8d6d5c1b
wangc <zzzs1991@gmail.com>
2023-02-01 11:12:31 +0800

    leet_563

---
+++ leet_0563/SumOfLeftLeaves.java
@@ -0,0 +1,83 @@
+package leet_0563;
+
+import java.util.Objects;
+
+public class SumOfLeftLeaves {
+
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+
+        Assembler assembler1 = new Assembler(new Integer[] { 1,2,3 });
+        TreeNode root1 = assembler1.assembleTree(0);
+        System.out.println(solution.findTilt(root1));
+
+        Solution solution2 = new Solution();
+        Assembler assembler2 = new Assembler(new Integer[] { 4,2,9,3,5,null,7 });
+        TreeNode root2 = assembler2.assembleTree(0);
+        System.out.println(solution2.findTilt(root2));
+
+    }
+
+    public static class Assembler {
+        private Integer[] source;
+
+        public Assembler() {
+        }
+
+        public Assembler(Integer[] source) {
+            this.source = source;
+        }
+
+        private TreeNode assembleTree(int target) {
+            assert target >= 0;
+            // 从数组中取出目标值 并创建节点
+            if (target >= source.length || Objects.isNull(source[target]))
+                return null;
+            TreeNode cur = new TreeNode();
+            cur.val = source[target];
+            cur.left = assembleTree(target * 2 + 1);
+            cur.right = assembleTree(target * 2 + 2);
+            return cur;
+        }
+    }
+
+    // Definition for a binary tree node.
+    public static class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode() {
+        }
+
+        TreeNode(int val) {
+            this.val = val;
+        }
+
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+
+    public static class Solution {
+        int sum = 0;
+
+        public int findTilt(TreeNode root) {
+            dfs(root);
+            return sum;
+        }
+
+        private int dfs(TreeNode root) {
+            if (root == null) {
+                return 0;
+            }
+            int left = dfs(root.left);
+            int right = dfs(root.right);
+            sum += Math.abs(left - right);
+            return root.val + left + right;
+        }
+    }
+
+}
